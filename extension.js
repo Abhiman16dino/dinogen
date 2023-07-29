ad6c641c58c1926bf10df43fb3b0cb6e3e637a05
abhishek.mondal@sap.com <abhishek.mondal@sap.com>
2023-07-30 02:45:46 +0530

    Added: Output is now coming to editor

--- extension.js
+++ extension.js
@@ -1,10 +1,11 @@
 const vscode = require('vscode');
 const genJS = require('./gen');
+const { info } = require('console');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-  function activate(context) {
+function activate(context) {
 
 	console.log('Congratulations, your extension "dinogen" is now active!');
 
@@ -14,39 +15,45 @@ const genJS = require('./gen');
 		//Get the active editor
 		const editor = vscode.window.activeTextEditor;
 
-		if(editor){
+		if (editor) {
 			const selectedText = editor.document.getText(editor.selection);
+			const position = editor.selection.end
 
-				vscode.window.withProgress(
-					{
-						location: vscode.ProgressLocation.Window,
-						cancellable: false,
-						title: "Genrating data..."
-					}, async (progress) => {
-						progress.report({increment: 0});
-						const data = await genJS.genJS(selectedText);
-						console.log(data)
-						vscode.window.showInformationMessage(`Response: ${data}`);
-						progress.report({increment:100});
-					}
-				);
+			vscode.window.withProgress(
+				{
+					location: vscode.ProgressLocation.Window,
+					cancellable: false,
+					title: "Genrating data..."
+				}, async (progress) => {
+					progress.report({ increment: 0 });
+					const data = await genJS.genJS(selectedText);
+					let output = data[0].candidates[0].output
+					let infoStat = data[0].candidates[0].safetyRatings
+					
+						vscode.window.showInformationMessage(`Response: ${JSON.stringify(infoStat)}`);
+						editor.edit((editBuilder) => {
+							editBuilder.insert(position, '\n\n' + output);
+						});
+					progress.report({ increment: 100 });
+				}
+			);
 		}
 	});
 
-	 // Add the context menu item to the editor
-	 let disposableContextMenu = vscode.languages.registerCodeActionsProvider('*', {
+	// Add the context menu item to the editor
+	let disposableContextMenu = vscode.languages.registerCodeActionsProvider('*', {
 		provideCodeActions(document, range) {
-		  const showSelectedTextAction = new vscode.CodeAction('Show Selected Text', vscode.CodeActionKind.Empty);
-		  showSelectedTextAction.command = { command: 'dinogen.helloWorld', title: 'Show Selected Text' };
-		  return [showSelectedTextAction];
+			const showSelectedTextAction = new vscode.CodeAction('Show Selected Text', vscode.CodeActionKind.Empty);
+			showSelectedTextAction.command = { command: 'dinogen.helloWorld', title: 'Show Selected Text' };
+			return [showSelectedTextAction];
 		}
-	  });
+	});
 
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(disposableContextMenu);
 }
 
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
--- gen.js
+++ gen.js
@@ -1,28 +1,28 @@
 async function genJS(prompt) {
 
-const { TextServiceClient } =
-  require("@google-ai/generativelanguage").v1beta2;
+  const { TextServiceClient } =
+    require("@google-ai/generativelanguage").v1beta2;
 
-const { GoogleAuth } = require("google-auth-library");
+  const { GoogleAuth } = require("google-auth-library");
 
-const API_KEY = "AIzaSyB1rxyM4hj9Apyg8Xy-WpcZ_7snvfpP-L0"
+  const API_KEY = "AIzaSyB1rxyM4hj9Apyg8Xy-WpcZ_7snvfpP-L0"
 
-const MODEL_NAME = "models/text-bison-001";
+  const MODEL_NAME = "models/text-bison-001";
 
-const client = new TextServiceClient({
-  authClient: new GoogleAuth().fromAPIKey(API_KEY),
-});
+  const client = new TextServiceClient({
+    authClient: new GoogleAuth().fromAPIKey(API_KEY),
+  });
 
-//const prompt = "Repeat after me: one, two,";
-let data = await client
-.generateText({
-  model: MODEL_NAME,
-  prompt: {
-    text: prompt,
-  },
-});
-
- return data[0].candidates[0].output
+  //const prompt = "Repeat after me: one, two,";
+  let data = await client
+    .generateText({
+      model: MODEL_NAME,
+      prompt: {
+        text: prompt,
+      },
+    });
+  console.log(data)
+  return data
 }
 
 module.exports = { genJS }
