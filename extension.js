f4a94f8708c61feb57c24d1fa5a22fd497376343
abhishek.mondal@sap.com <abhishek.mondal@sap.com>
2023-07-30 01:46:10 +0530

    Added: PALM API, Progress Bar, Handled Async Behaviour

--- extension.js
+++ extension.js
@@ -1,33 +1,53 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require('vscode');
+const genJS = require('./gen');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+  function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "dinogen" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dinogen.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('dinogen.helloWorld', async function () {
+		//vscode.window.showInformationMessage('Hello World from DinoGen!');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DinoGen!');
+		//Get the active editor
+		const editor = vscode.window.activeTextEditor;
+
+		if(editor){
+			const selectedText = editor.document.getText(editor.selection);
+
+				vscode.window.withProgress(
+					{
+						location: vscode.ProgressLocation.Window,
+						cancellable: false,
+						title: "Genrating data..."
+					}, async (progress) => {
+						progress.report({increment: 0});
+						const data = await genJS.genJS();
+						vscode.window.showInformationMessage(`Selected Text: ${data}`);
+						progress.report({increment:100});
+					}
+				);
+
+			
+		}
 	});
 
+	 // Add the context menu item to the editor
+	 let disposableContextMenu = vscode.languages.registerCodeActionsProvider('*', {
+		provideCodeActions(document, range) {
+		  const showSelectedTextAction = new vscode.CodeAction('Show Selected Text', vscode.CodeActionKind.Empty);
+		  showSelectedTextAction.command = { command: 'dinogen.helloWorld', title: 'Show Selected Text' };
+		  return [showSelectedTextAction];
+		}
+	  });
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableContextMenu);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
---
+++ gen.js
@@ -0,0 +1,28 @@
+async function genJS() {
+
+const { TextServiceClient } =
+  require("@google-ai/generativelanguage").v1beta2;
+
+const { GoogleAuth } = require("google-auth-library");
+
+const API_KEY = "AIzaSyB1rxyM4hj9Apyg8Xy-WpcZ_7snvfpP-L0"
+
+const MODEL_NAME = "models/text-bison-001";
+
+const client = new TextServiceClient({
+  authClient: new GoogleAuth().fromAPIKey(API_KEY),
+});
+
+const prompt = "Repeat after me: one, two,";
+let data = await client
+.generateText({
+  model: MODEL_NAME,
+  prompt: {
+    text: prompt,
+  },
+});
+
+ return data[0].candidates[0].output
+}
+
+module.exports = { genJS }
